62fd01fc2f19ea4042772484184a2a8cc103eb9a
yilinzou <yz7017@nyu.edu>
2022-05-01 17:24:03 -0400

    change

--- breakoDemo/googleSearch/script.js
+++ breakoDemo/googleSearch/script.js
@@ -1,12 +1,14 @@
 let canvas1 = document.getElementById("canvas1");
 const ctx1 = canvas1.getContext("2d");
-canvas1.width = window.innerWidth;
-canvas1.height = window.innerHeight;
+// canvas1.width = window.innerWidth;
+// canvas1.height = window.innerHeight;
+canvas1.width = '500px';
+canvas1.height = '500px';
 
 // ctx1.clearRect(0, 0, canvas1.width, canvas1.height);
 ctx1.beginPath();
 ctx1.lineWidth = "4";
 ctx1.strokeStyle = "green";
-ctx1.rect(20, 20, 150, 100);
+ctx1.rect(20, 20, 50, 50);
 ctx1.fill();
 ctx1.stroke();
