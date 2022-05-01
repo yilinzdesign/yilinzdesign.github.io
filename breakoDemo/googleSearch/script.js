d0f565bff3d62a880d9dd1b4b7161001f14eb7ca
yilinzou <yz7017@nyu.edu>
2022-05-01 17:20:06 -0400

    Update script.js

--- breakoDemo/googleSearch/script.js
+++ breakoDemo/googleSearch/script.js
@@ -5,6 +5,8 @@ canvas1.height = window.innerHeight;
 
 // ctx1.clearRect(0, 0, canvas1.width, canvas1.height);
 ctx1.beginPath();
+ctx1.lineWidth = "4";
+ctx1.strokeStyle = "green";
 ctx1.rect(20, 20, 150, 100);
 ctx1.fill();
 ctx1.stroke();
