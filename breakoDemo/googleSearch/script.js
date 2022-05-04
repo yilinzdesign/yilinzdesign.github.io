2537070c3f9dee03c7e17079e75140a0a38d5e27
yilinzou <yz7017@nyu.edu>
2022-05-04 17:07:36 -0400

    new

--- breakoDemo/googleSearch/script.js
+++ breakoDemo/googleSearch/script.js
@@ -1,24 +1,14 @@
 
-// Copyright (c) 2019 ml5
-//
-// This software is released under the MIT License.
-// https://opensource.org/licenses/MIT
 
-/* ===
-ml5 Example
-PoseNet using p5.js
-=== */
-/* eslint-disable */
-
-// Grab elements, create settings, etc.
 var video = document.getElementById("video");
 var canvas = document.getElementById("canvas2");
 var ctx = canvas.getContext("2d");
+let i = 0;
+let next = 0;
 
-// The detected positions will be inside an array
 let poses = [];
+let pose;
 
-// Create a webcam capture
 if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
   navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
     video.srcObject = stream;
@@ -26,19 +16,15 @@ if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
   });
 }
 
-// A function to draw the video and poses into the canvas.
-// This function is independent of the result of posenet
-// This way the video will not seem slow if poseNet
-// is not detecting a position
 function drawCameraIntoCanvas() {
-  // Draw the video element into the canvas
-  ctx.drawImage(video, 0, 0, 640, 480);
-  // We can call both functions to draw all keypoints and the skeletons
+  video.onload = function up(){
+    ctx.drawImage(video, 0, 0, 640, 480);
+}
+
   drawKeypoints();
-  drawSkeleton();
+  // drawSkeleton();
   window.requestAnimationFrame(drawCameraIntoCanvas);
 }
-// Loop over the drawCameraIntoCanvas function
 drawCameraIntoCanvas();
 
 // Create a new poseNet method with a single detection
@@ -48,29 +34,44 @@ poseNet.on("pose", gotPoses);
 // A function that gets called every time there's an update from the model
 function gotPoses(results) {
   poses = results;
+  
+  // console.log(poses);
+  if(poses.length>0){
+    pose = poses[0].poses
+  }
 }
 
 function modelReady() {
-  console.log("model ready");
+  // console.log("model ready");
   poseNet.multiPose(video);
 }
 
 // A function to draw ellipses over the detected keypoints
 function drawKeypoints() {
-  // Loop through all the poses detected
-  for (let i = 0; i < poses.length; i += 1) {
-    // For each pose detected, loop through all the keypoints
-    for (let j = 0; j < poses[i].pose.keypoints.length; j += 1) {
-      let keypoint = poses[i].pose.keypoints[j];
-      // Only draw an ellipse is the pose probability is bigger than 0.2
-      if (keypoint.score > 0.2) {
-        ctx.beginPath();
-        ctx.arc(keypoint.position.x, keypoint.position.y, 10, 0, 2 * Math.PI);
-        ctx.stroke();
-      }
-    }
-  }
+      if(poses.length>0){
+      ctx.beginPath();
+      ctx.arc(poses[0].pose.nose.x, poses[0].pose.nose.y, 10, 0, 2 * Math.PI);
+      // ctx.moveTo(poses[0].pose.nose.x, poses[0].pose.nose.y);
+      // ctx.lineTo(poses[0].pose.nose.x, poses[0].pose.nose.y);
+      ctx.stroke();
+      
+      //going right
+      setInterval(function recheckposition(){
+        show(0);
+        if (poses[0].pose.nose.x <300){
+          // console.log("turning right")
+          changeToRight();
+          console.log(next);
+          // setTimeout(changeToRight(), 10000);
+        }
+      }, 8000)
+      
+      //going left
+    } 
+    
+
 }
+// }
 
 // A function to draw the skeletons
 function drawSkeleton() {
@@ -86,10 +87,8 @@ function drawSkeleton() {
       ctx.stroke();
     }
   }
+  
 }
-
-
-
 
 let canvas1 = document.getElementById("canvas1");
 let color = "red";
@@ -98,21 +97,79 @@ const ctx1 = canvas1.getContext("2d");
 canvas1.width = window.innerWidth;
 canvas1.height = window.innerHeight;
 // ctx1.clearRect(0, 0, canvas1.width, canvas1.height);
+
+
+// let about = ctx1.rect(20, 20, 50, 50);
+// let store = ctx1.rect(80,20,50,50);
+// let gmail = ctx1.rect(1150,20,50,50);
+// let images = ctx1.rect(1200,20,50,50);
+// let menu = ctx1.rect(1260,20,50,50);
+// let login = ctx1.rect(1320,20,60,50);
+// let typein = ctx1.rect(420,260,600,50);
+// let googlesearch = ctx1.rect(590,330,120,50);
+// let Imfeelinglucky = ctx1.rect(720,330,120,50);
+
+// let selection = [about, store, gmail, images, menu, login, typein, googlesearch, Imfeelinglucky];
+function changeToRight()
+{
+  ctx1.clearRect(0,0,canvas1.width,canvas1.height);
+  if(next<9){
+    next= next + 1
+  } else if(next >8){
+    next = 8;
+  }
+  show(next);
+  // console.log(next)
+}
+function show(i){
 ctx1.beginPath();
 ctx1.lineWidth = "2";
 ctx1.strokeStyle = color;
-let about = ctx1.rect(20, 20, 50, 50);
-let store = ctx1.rect(80,20,50,50);
-let gmail = ctx1.rect(1150,20,50,50);
-let images = ctx1.rect(1200,20,50,50);
-let menu = ctx1.rect(1260,20,50,50);
-let login = ctx1.rect(1320,20,60,50);
-let typein = ctx1.rect(420,260,600,50);
-let googlesearch = ctx1.rect(590,330,120,50);
-let Imfeelinglucky = ctx1.rect(720,330,120,50);
+  if(i===0){
+    ctx1.clearRect(0,0,canvas1.width, canvas1.height);
+    ctx1.rect(20, 20, 50, 50);//draw about
+    ctx1.stroke();
+  }else if(i===1){
+    ctx1.clearRect(0,0,canvas1.width, canvas1.height);
+    ctx1.rect(80,20,50,50);//draw store
+    ctx1.stroke();
+  } else if(i===2){
+    ctx1.clearRect(0,0,canvas1.width, canvas1.height);
+    ctx1.rect(1150,20,50,50);
+    ctx1.stroke();
+  }else if(i===3){
+    ctx1.clearRect(0,0,canvas1.width, canvas1.height);
+    ctx1.rect(1200,20,50,50);
+    ctx1.stroke();
+  }else if (i===4){
+    ctx1.clearRect(0,0,canvas1.width, canvas1.height);
+    ctx1.rect(1260,20,50,50);
+    ctx1.stroke();
+  }else if (i===5){
+    ctx1.clearRect(0,0,canvas1.width, canvas1.height);
+    ctx1.rect(1320,20,60,50);
+    ctx1.stroke();
+  }else if (i===6){
+    ctx1.clearRect(0,0,canvas1.width, canvas1.height);
+    ctx1.rect(420,260,600,50);
+    ctx1.stroke();
+  }else if (i===7){
+    ctx1.clearRect(0,0,canvas1.width, canvas1.height);
+    ctx1.rect(590,330,120,50);
+    ctx1.stroke();
+  }else if (i===8){
+    ctx1.clearRect(0,0,canvas1.width, canvas1.height);
+    ctx1.rect(720,330,120,50);
+    ctx1.stroke();
+  }
+  
+
+}
 
 
-ctx1.stroke();
+
+
+
 
 
 
